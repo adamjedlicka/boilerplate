fdd9d6c09b5ce277fdf93a032ce6a3f21e25d291
Adam Jedlička <mail@adamjedlicka.cz>
2026-06-05 13:06:08 +0200

    Add server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [
+		nitro(),
 		tanstackRouter({
 			target: 'react',
 			routesDirectory: './app/pages',
